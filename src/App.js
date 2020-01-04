f24dc86f7747175da9973e2c66a797ee234a96ad
Kadyr Poyraz <poyraz_2001@mail.ru>
2020-01-04 08:06:42 +0200

    Установил bootstrap и reactstrap в проект

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = null;
+
+  async componentDidMount() {
+    const resp = await axios('https://aws.random.cat/meow');
+    console.log(resp.data);
+    this.setState(resp.data);
+  }
+
+  render() {
+        return this.state && (
+            <div>
+              <img src={this.state.file} alt=""/>
+            </div>
+        );
+    }
 }
 
 export default App;
